cfa01709063a80174205b1f2753044ec3f4ee7e6
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-31 15:12:23 +0100

    a few adjustments

--- A12/Azure_AD_Sync_Hardmatch.ps1
+++ A12/Azure_AD_Sync_Hardmatch.ps1
@@ -1,11 +1,17 @@
+#Create a variable for the user
 $user = "sina.meier"
 
+#Create the UPN variable
 $userUPN = "sina.meier@tomsazure.ch"
 
+#We need the guid
 $guid = [guid]((Get-ADUser -Identity "$user").objectGuid)
 
+#Convert to Base64
 $immutableId = [System.Convert]::ToBase64String($guid.ToByteArray())
 
+#Connect to the Microsoft 365
 Connect-MsolService
 
+#Set the ImmutableId
 Get-MsolUser -UserPrincipalName $userUPN | Set-MsolUser -ImmutableId $immutableId
